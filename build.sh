c2393e20c89706d0e0cceb609de10bcb8c752765
Tiago Ilieve <tiago.myhro@gmail.com>
2020-03-28 02:06:29 +0000

    Reduce binary size with proper ldflags

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ TARBALL="https://github.com/cloudflare/cloudflared/archive/${VERSION}.tar.gz"
 
 curl -sL "$TARBALL" | tar zxf -
 cd "cloudflared-$VERSION/" || exit 1
-go build -o /go/bin/cloudflared ./cmd/cloudflared/
+go build -ldflags "-s -w" -o /go/bin/cloudflared ./cmd/cloudflared/
